61091bef7a7a8012da26c084d97ee7c5bd979d1d
crobualr <60245615+crobualr@users.noreply.github.com>
2020-03-22 23:24:46 -0500

    Create Database.cpp

---
+++ Quest--2/Database.cpp
@@ -0,0 +1,22 @@
+#include "Database.h"
+#include <algorithm>
+
+Database::Database() {}
+
+void Database::append(Account const& a) {
+	Database::accounts.push_back(a);
+}
+
+bool Database::find(Account const& account) {
+	//vector<Account>::iterator it = accounts.begin(); it != accounts.end(); it++) 
+	/*vector<Account>::iterator it = std::find(accounts.begin(), accounts.end(), account);
+	if (it != accounts.end()) {
+		return true;
+	}*/
+
+	return false;
+}
+
+vector<Account> Database::getAccounts() const {
+	return accounts;
+}
