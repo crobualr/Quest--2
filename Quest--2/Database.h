c3854ccd47b1cab9b7ebc31b71151be75db33d8d
crobualr <60245615+crobualr@users.noreply.github.com>
2020-03-23 11:47:09 -0500

    Update Database.h

--- Quest--2/Database.h
+++ Quest--2/Database.h
@@ -9,7 +9,7 @@ public:
 	void append(Account const& a);
 	bool find(Account const& account);
 	float findBalance(int ID, int Pin);
-	Account findAccount(int ID, int Pin);
+	Account& findAccount(int ID, int Pin);
 	vector<Account>getAccounts() const;
 private:
 	vector<Account>accounts;
