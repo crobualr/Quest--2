ce1b119c2afbe09f10703cd8de5befb303fc2525
crobualr <60245615+crobualr@users.noreply.github.com>
2020-03-22 23:25:10 -0500

    Create Database.h

---
+++ Quest--2/Database.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+#include<iostream>
+#include "Account.h"
+using namespace std;
+class Database {
+public:
+	Database();
+	void append(Account const& a);
+	bool find(Account const& account);
+	vector<Account>getAccounts() const;
+private:
+	vector<Account>accounts;
+};
