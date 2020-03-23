94367fbb1542673549702213de8999aa43229e42
crobualr <60245615+crobualr@users.noreply.github.com>
2020-03-22 23:43:46 -0500

    Update Database.cpp

--- Quest--2/Database.cpp
+++ Quest--2/Database.cpp
@@ -9,10 +9,10 @@ void Database::append(Account const& a) {
 
 bool Database::find(Account const& account) {
 	//vector<Account>::iterator it = accounts.begin(); it != accounts.end(); it++) 
-	/*vector<Account>::iterator it = std::find(accounts.begin(), accounts.end(), account);
+	vector<Account>::iterator it = std::find(accounts.begin(), accounts.end(), account);
 	if (it != accounts.end()) {
 		return true;
-	}*/
+	}
 
 	return false;
 }
